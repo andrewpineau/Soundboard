912ad18736d03b0d9618b5f2de951f6c7e9a4197
Andrew Pineau <andrewpineau@hotmail.com>
2016-04-09 11:32:03 -0400

    Data models added, menu UI started.

--- Soundboard/Soundboard/MainPage.xaml.cs
+++ Soundboard/Soundboard/MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Soundboard.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -17,24 +19,28 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Soundboard
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ObservableCollection<Sound> sounds = new ObservableCollection<Sound>();
+        private List<MenuItem> MenuItems = new List<MenuItem>();
         public MainPage()
         {
             this.InitializeComponent();
+            SoundManager.GetAllSounds(sounds);
+            MenuItems.Add(new MenuItem { IconFile = "/Assets/Icons/animals.png", Category = SoundCategory.Animals});
+            MenuItems.Add(new MenuItem { IconFile = "/Assets/Icons/cartoon.png", Category = SoundCategory.Cartoons });
+            MenuItems.Add(new MenuItem { IconFile = "/Assets/Icons/taunt.png", Category = SoundCategory.Taunts });
+            MenuItems.Add(new MenuItem { IconFile = "/Assets/Icons/warning.png", Category = SoundCategory.Warnings });
         }
 
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
-
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
         }
 
         private void SearchAutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
         {
-
+           
         }
 
         private void SearchAutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
---
+++ Soundboard/Soundboard/Models/SoundManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soundboard.Models
+{
+    public class SoundManager
+    {
+
+        public static void GetAllSounds(ObservableCollection<Sound> observableSounds)
+        {
+            var sounds = GetSounds();
+            observableSounds.Clear();
+
+            sounds.ForEach(s => observableSounds.Add(s));
+        }
+
+        public static void GetSoundsByCategory(ObservableCollection<Sound> observableSounds, SoundCategory category)
+        {
+            var allSounds = GetSounds();
+            var filteredSounds = allSounds.Where(p => p.Category == category).ToList();
+            observableSounds.Clear();
+            filteredSounds.ForEach(s => observableSounds.Add(s));
+        }
+        private static List<Sound> GetSounds()
+        {
+            var sounds = new List<Sound>();
+
+            sounds.Add(new Sound("Cow", SoundCategory.Animals));
+            sounds.Add(new Sound("Cat", SoundCategory.Animals));
+
+            sounds.Add(new Sound("Gun", SoundCategory.Cartoons));
+            sounds.Add(new Sound("Spring", SoundCategory.Cartoons));
+
+            sounds.Add(new Sound("Clock", SoundCategory.Taunts));
+            sounds.Add(new Sound("LOL", SoundCategory.Taunts));
+
+            sounds.Add(new Sound("Ship", SoundCategory.Warnings));
+            sounds.Add(new Sound("Siren", SoundCategory.Warnings));
+
+            return sounds;
+        }
+    }
+}
